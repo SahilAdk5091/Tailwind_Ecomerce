5ad88ddc75e0f286ed5f6bc5a4c21d58d6797a90
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-05-27 00:10:57 +0545

    Adding Footer Section

--- ecommerce/src/modules/Home/index.js
+++ ecommerce/src/modules/Home/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hero from '../../components/Hero'
 import Products from '../../components/Products'
+import Footer from '../../components/Footer'
 
 const Home = () => {
   return (
     <>
         <Hero/>
+        <h2 className='text-4xl font-bold text-center'>Products</h2>
         <Products/>
+        <Footer/>
     </>
   )
 }
