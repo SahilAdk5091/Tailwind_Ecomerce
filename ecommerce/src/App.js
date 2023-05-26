bf249d05a25b0a6cd1c198273525f0dead1584e9
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-05-26 23:38:11 +0545

    First Commit

---
+++ ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className='text-white-800 text-4xl'>
+    <Header/>
+    </div>
+  );
+}
+
+export default App;
