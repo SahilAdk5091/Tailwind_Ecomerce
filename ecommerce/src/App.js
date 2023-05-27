1d7c659bd9e162d0ff4a90699d1855b081e0c2b0
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-05-27 17:06:39 +0545

    Adding Products find by id

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Home from './modules/Home';
+import { Route, Routes } from 'react-router-dom'
+import Product from './components/Product';
 
 function App() {
   return (
     <div>
     <Header/>
-    <Home/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/products/:id' element={<Product/>}/>
+    </Routes>
+   
     </div>
   );
 }
