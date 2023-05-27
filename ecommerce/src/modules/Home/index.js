bb61ead3a24b6162ba44862b8ce3c39e27c65904
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-05-27 10:57:00 +0545

    Adding Feature Section

--- ecommerce/src/modules/Home/index.js
+++ ecommerce/src/modules/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../../components/Hero'
 import Products from '../../components/Products'
 import Footer from '../../components/Footer'
+import Feature from '../../components/Feature'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero/>
         <h2 className='text-4xl font-bold text-center'>Products</h2>
         <Products/>
+        <Feature/>
         <Footer/>
     </>
   )
