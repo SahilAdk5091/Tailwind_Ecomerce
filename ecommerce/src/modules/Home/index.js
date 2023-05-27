0d79b6c15a1cb3eb35cbf7010d5c26540aa0a7ce
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-05-27 12:56:08 +0545

    Adding Products in product page using  Fakeapistore

--- ecommerce/src/modules/Home/index.js
+++ ecommerce/src/modules/Home/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../../components/Hero'
 import Products from '../../components/Products'
 import Footer from '../../components/Footer'
 import Feature from '../../components/Feature'
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(()=>{
+    const fetchProducts = async () =>{
+      const response = await fetch('https://fakestoreapi.com/products')
+      const data = await response.json()
+      console.log(data);
+      setProducts(data);
+    }
+    fetchProducts()
+  },[])
   return (
     <>
         <Hero/>
         <h2 className='text-4xl font-bold text-center'>Products</h2>
+        {
+          products.length>0 ?
+          <Products products={products} />
+          :
+          <div>Loading...</div>
+        }
         <Products/>
+        <h2 className='text-4xl font-bold text-center'>Popular Feature</h2>
         <Feature/>
         <Footer/>
     </>
